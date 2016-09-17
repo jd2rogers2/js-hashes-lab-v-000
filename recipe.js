db97fbb3760d9f3add626a8d9467ea617e5d0be2
jd2rogers2 <jd2rogers2@gmail.com>
2016-09-17 16:34:36 -0400

    donzo

--- recipe.js
+++ recipe.js
@@ -1,1 +1,22 @@
 'use strict';
+
+function addIngredient(recipe_hash, ingredient, num) {
+  recipe_hash[ingredient] = num;
+  return recipe_hash;
+}
+
+function removeIngredient(recipe_hash, ingredient) {
+  delete recipe_hash[ingredient];
+  return recipe_hash;
+}
+
+function updateIngredient(recipe_hash, ingredient, num) {
+  recipe_hash[ingredient] = num;
+  return recipe_hash;
+}
+
+function readRecipe(recipe_hash) {
+  for (var ingredient in recipe_hash) {
+    console.log("this recipe calls for " + recipe_hash[ingredient] + " of " + ingredient );
+  }
+}
